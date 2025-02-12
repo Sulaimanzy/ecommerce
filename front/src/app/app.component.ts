3ed08d6e3e9255dfd4aaf2035c549417cbfeeb18
Sulaiman Bah <bah.sulaiman@hotmail.com>
2025-02-13 00:13:24 +0100

    add main-header component

--- front/src/app/app.component.ts
+++ front/src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { TopHeaderComponent } from './top-header/top-header.component';
+import { MainHeaderComponent } from './main-header/main-header.component';
 
 @Component({
   selector: 'app-root',
-  imports: [TopHeaderComponent],
+  imports: [TopHeaderComponent, MainHeaderComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
